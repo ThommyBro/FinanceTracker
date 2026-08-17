d88a51767695928efb2d34af5c1ac095da87c86b
Thomas Brockt <thomas.brockt@gmail.com>
2026-08-17 12:26:17 +0200

    enum categories add

---
+++ pyFinance/category.py
@@ -0,0 +1,16 @@
+from enum import Enum, auto
+
+class Category(Enum):
+    HOUSING = auto()
+    FOOD = auto()
+    TRANSPORT = auto()
+    ENTERTAINMENT = auto()
+    HEALTH = auto()
+    EDUCATION = auto()
+    CLOTHING = auto()
+    SALARY = auto()
+    FREELANCE = auto()
+    INVESTMENT = auto()
+    OTHER = auto()
+
+    
---
+++ pyFinance/transaction_type.py
@@ -0,0 +1,5 @@
+from enum import Enum, auto
+
+class Transaction(Enum):
+    INCOME = auto()
+    EXPENSE = auto()
