e7d413c5d355dd541b294b26148d06d87473377c
Thomas Brockt <thomas.brockt@gmail.com>
2026-09-21 12:07:52 +0200

    add factory for transactions

--- tests/test_account.py
+++ tests/test_account.py
@@ -8,6 +8,7 @@ from src.pyFinance.account import Account
 
 
 
+# 1 simple EXPENSE transaction
 @pytest.fixture
 def expense_transaction():
     return Transaction(
@@ -18,7 +19,7 @@ def expense_transaction():
         "2026-08-17"
     )
 
-
+# 1 simple INCOME transaction
 @pytest.fixture
 def income_transaction():
     return Transaction(
@@ -26,8 +27,51 @@ def income_transaction():
         1000,
         TransactionType.INCOME,
         Category.SALARY,
-        "2026-08-17"
+        "2026-09-17"
     )
+
+
+
+def make_transaction(
+    description: str = "Test Transaction",
+    amount: float = 100.0,
+    transaction_type: TransactionType = TransactionType.EXPENSE,
+    category: Category = Category.FOOD,
+    date: str = "2026-09-01",
+    tags: set[str] | None = None,
+) -> Transaction:
+    """
+    Factory for transactions
+    """
+    return Transaction(
+        description=description,
+        amount=amount,
+        transaction_type=transaction_type,
+        category=category,
+        date=date,
+        tags=tags or set(),
+    )
+
+# Use factory for lots of transactions
+# 
+transactions = [
+    make_transaction("Rent", 900, date="2026-09-01", category=Category.HOUSING),
+    make_transaction("Food", 120, date="2026-09-03"),
+    make_transaction("Food", 300, date="2026-09-10"),
+    make_transaction("Cinema", 35, date="2026-09-15", category=Category.ENTERTAINMENT),
+    make_transaction("Shoes", 140, date="2026-09-20", category=Category.CLOTHING),
+    make_transaction("Doctor", 80, date="2026-10-01", category=Category.HEALTH),
+    make_transaction("New Bike", 1300,date="2026-10.05", category=Category.OTHER)
+]
+
+salary = make_transaction(
+    description="Salary",
+    amount=3000,
+    transaction_type=TransactionType.INCOME,
+    category=Category.SALARY,
+    date="2026-09-30",
+)
+
 
 def test_empty_account():
     acc = Account(name="My Account")
@@ -69,6 +113,22 @@ def test_search(income_transaction,expense_transaction):
     acc.add_transaction(expense_transaction)
 
     assert acc.search("income") == [income_transaction]
+
+
+def test_filter_by_month(income_transaction, expense_transaction):
+    acc = Account(name="My Account")
+
+    acc.add_transaction(income_transaction)
+    acc.add_transaction(expense_transaction)
+
+    assert income_transaction.date == "2026-09-17"
+    assert expense_transaction.date == "2026-08-17"
+
+    august = acc.filter_by_month("2026-08")
+    september = acc.filter_by_month("2026-09")
+
+    assert august == [expense_transaction]
+    assert september == [income_transaction]
     
   
 
