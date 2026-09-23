59c42fb75f3464263f8e99b3063d3e1bcd2d4717
Thomas Brockt <thomas.brockt@gmail.com>
2026-09-23 15:44:29 +0200

    add all methods

--- src/pyFinance/budget_tracker.py
+++ src/pyFinance/budget_tracker.py
@@ -19,12 +19,14 @@ class Tracker:
 
 
     def set_budget(self, category: Category, monthly_limit: float, month: str) -> None:
-        ...
+        for budget in self.budgets:
+            if category == budget.category and month == budget.month:
+                budget.monthly_limit = monthly_limit
 
 
-    def check_budget(self, account: Account) -> list[tuple[Budget, float]]:
-        ...
+    def check_budgets(self, account: Account) -> list[tuple[Budget, float]]:
+        return [(b, b.usage_percentage(account)) for b in self.budgets]
 
 
     def exceeded_budgets(self, account: Account) -> list[Budget]:
-        ...
+        return [b for b in self.budgets if b.is_exceeded(account)]
